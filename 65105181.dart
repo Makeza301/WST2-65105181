6016e56bd4c3eedefabbb557d99f861a82865949
Makeza301 <157354845+Makeza301@users.noreply.github.com>
2024-06-28 11:44:25 +0700

    Create 65105181.dart

---
+++ 65105181.dart
@@ -0,0 +1,96 @@
+class Person {
+  String _name;
+
+  Person(this._name);
+
+  String get name => _name;
+  set name(String value) => _name = value;
+}
+
+class Engine {
+  String _model = "";
+  double _speed = 0;
+
+  Engine(this._model, this._speed);
+
+  String get model => _model;
+  set model(String value) => _model = value;
+
+  double get speed => _speed;
+  set speed(double value) => _speed = value;
+
+  void displayEngineInfo() {
+    print('Model: $_model, Speed: $_speed km/h');
+  }
+}
+
+class Car {
+  String _brand ="";
+  String _model = "";
+  Person _owner;
+  Engine _engine;
+
+  Car(this._brand, this._model, this._owner, this._engine);
+
+  String get brand => _brand;
+  set brand(String value) => _brand = value;
+
+  String get model => _model;
+  set model(String value) => _model = value;
+
+  Person get owner => _owner;
+  set owner(Person value) => _owner = value;
+
+  Engine get engine => _engine;
+  set engine(Engine value) => _engine = value;
+
+  void displayCarInfo() {
+    print('Brand: $_brand, Model: $_model, Owner: ${_owner.name}, Engine: ${_engine.model}');
+  }
+
+  void run() {
+    print('Running at ${_engine.speed} km/h');
+  }
+}
+
+class Honda extends Car {
+  String _color;
+
+  Honda(String brand, String model, Person owner, Engine engine, this._color)
+      : super(brand, model, owner, engine);
+
+  String get color => _color;
+  set color(String value) => _color = value;
+
+  @override
+  void displayCarInfo() {
+    print('Brand: $brand, Model: $model, Color: $_color, Owner: ${owner.name}, Engine: ${engine.model}');
+  }
+
+  @override
+  void run() {
+    print('Running at 120 km/h');
+  }
+}
+
+void main() {
+  // Create a person
+  Person owner = Person('Alice');
+
+  // Create an engine
+  Engine engine = Engine('V8', 200.0);
+
+  // Create a car
+  Car car = Car('Toyota', 'Corolla', owner, engine);
+
+  // Display car information and run the car
+  car.displayCarInfo();
+  car.run();
+
+  // Create a Honda car
+  Honda honda = Honda('Honda', 'Civic', owner, engine, 'Red');
+
+  // Display Honda car information and run the Honda car
+  honda.displayCarInfo();
+  honda.run();
+}
